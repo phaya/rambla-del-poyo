8d5410b0195333b9bda8c51d991e14afd75191e1
Pablo Haya Coll <pablohaya@gmail.com>
2024-11-06 12:07:14 +0100

    Aesthetic changes

--- scripts/crecimiento_exponencial.R
+++ scripts/crecimiento_exponencial.R
@@ -1,6 +1,8 @@
+library(here)
 library(tidyverse)
 library(lubridate)
 library(scales)
+
 
 # A partir de la gráfica publicada por la Cuenca Hidrográfica del Jucar (CHJ)
 # https://x.com/CHJucar/status/1853407411064730011
@@ -63,18 +65,31 @@ datos %>%
            label = "Caudal medio del río Ebro", 
            vjust = -1, hjust = -1, 
            color = "black", size = 3) +
+  annotate("text", x = ymd_hms("2024-10-29 17:00:00"), y = 10, 
+           label = "x28", 
+           vjust = -1, hjust = 0, 
+           color = "black", size = 8) +
   scale_y_continuous(trans = 'log10',
                      labels = function(x) paste0(x,  " m³/s")) +
   scale_x_datetime(date_breaks = "1 hour", date_labels = "%H:%M") +
   labs(title = "Caudal en la Rambla del Poyo el 29 de octubre de 2024",
-       subtitle = "Caudal registrado en intervalos de cinco minutos entre las 11:00 y las 18:55 representado en escala logarítmica\nSe resaltan las dos áreas donde tuvo más impacto el crecimiento exponencial del caudal del rio", 
+       subtitle = "Caudal registrado en intervalos de cinco minutos entre las 11:00 y las 18:55 representado en escala logarítmica.\nSe resaltan las dos áreas donde tuvo más impacto el crecimiento exponencial del caudal del rio.\nEntre las 16:00 y las 18:00 el caudal se multiplico por 28.", 
        caption = "Datos proporcionados por Cuenca Hidrográfica del Jucar / Datadista",
        x = "", 
        y = "") +
   theme_minimal() +
   theme(panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
         plot.title = element_text(face = "bold", size = 16),
         plot.subtitle = element_text(size = 12))
+
+ggsave(here("outputs/caudal_escala_logaritmica.png"), 
+       width = 25, height = 12, units = "cm", 
+       dpi = 300)
+
+ggsave(here("outputs/caudal_escala_logaritmica.jpg"), 
+       width = 25, height = 12, units = "cm", 
+       dpi = 300)
 
 # Los datos originales vienen cada 5 minutos
 # Para visualizar los incrementos nos quedamos con 
@@ -103,14 +118,25 @@ datos_filtrados %>%
   geom_line(color="gray") +
   geom_point(color = "darkblue",
              size = 2) +
+  geom_text(aes(label = paste0("x", round(Diferencia_Caudal, 1))), 
+            vjust = -1, size = 3, color = "darkblue") +
   scale_x_datetime(date_breaks = "15 min", date_labels = "%H:%M") +
   scale_y_continuous(labels = function(x) paste0("x", x)) +
   labs(title = "Incrementos de caudal en la Rambla del Poyo entre las 16:00 y las 18:45",
-       subtitle = "Cada punto representa cuantas veces iba más deprisa el caudal tomando como referencia los últimos 30 minutos\nPor ejemplo, a las 17:00 había aproximadamente el doble de caudal que a las 16:30", 
+       subtitle = "Cada punto representa cuantas veces iba más deprisa el caudal tomando como referencia los últimos 30 minutos.\nPor ejemplo, a las 17:00 había aproximadamente el doble de caudal que a las 16:30.", 
        caption = "Datos proporcionados por Cuenca Hidrográfica del Jucar / Datadista",
        x = "", 
        y = "") +
   theme_minimal() +
   theme(panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank(),
         plot.title = element_text(face = "bold", size = 16),
         plot.subtitle = element_text(size = 12))
+
+ggsave(here("outputs/incrementos_de_caudal.png"), 
+       width = 25, height = 12, units = "cm", 
+       dpi = 300)
+
+ggsave(here("outputs/incrementos_de_caudal.jpg"), 
+       width = 25, height = 12, units = "cm", 
+       dpi = 300)
